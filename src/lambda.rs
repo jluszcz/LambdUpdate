de8c1e15bdb4f43191da5b8c179320b65592653b
Jacob Luszcz <jacob.luszcz@gmail.com>
2026-02-21 08:33:41 -0500

    Use lambda_runtime::Error

--- src/lambda.rs
+++ src/lambda.rs
@@ -2,18 +2,15 @@ use jluszcz_rust_utils::lambda;
 use lambda_runtime::{LambdaEvent, service_fn};
 use lambdupdate::{APP_NAME, update};
 use serde_json::{Value, json};
-use std::error::Error;
-
-type LambdaError = Box<dyn Error + Send + Sync + 'static>;
 
 #[tokio::main]
-async fn main() -> Result<(), LambdaError> {
+async fn main() -> Result<(), lambda_runtime::Error> {
     let func = service_fn(function);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-async fn function(event: LambdaEvent<Value>) -> Result<Value, LambdaError> {
+async fn function(event: LambdaEvent<Value>) -> Result<Value, lambda_runtime::Error> {
     lambda::init(APP_NAME, module_path!(), false).await?;
 
     update(serde_json::from_value(event.payload)?).await?;
