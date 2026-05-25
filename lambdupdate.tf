ddabeddf5ae4e300c1024247f77525d1e835e19e
Jacob Luszcz <jacob.luszcz@gmail.com>
2026-05-25 16:47:40 -0400

    tf: remove async DLQ (#80)

--- lambdupdate.tf
+++ lambdupdate.tf
@@ -144,39 +144,6 @@ resource "aws_lambda_function" "lambdupdate" {
   memory_size   = 128
 }
 
-resource "aws_sqs_queue" "dlq" {
-  name                      = "lambdupdate-dlq"
-  message_retention_seconds = 1209600
-}
-
-data "aws_iam_policy_document" "dlq" {
-  statement {
-    actions   = ["sqs:SendMessage"]
-    resources = [aws_sqs_queue.dlq.arn]
-  }
-}
-
-resource "aws_iam_policy" "dlq" {
-  name   = "lambdupdate.dlq.${var.aws_region}"
-  policy = data.aws_iam_policy_document.dlq.json
-}
-
-resource "aws_iam_role_policy_attachment" "dlq" {
-  role       = aws_iam_role.lambdupdate.name
-  policy_arn = aws_iam_policy.dlq.arn
-}
-
-resource "aws_lambda_function_event_invoke_config" "lambdupdate" {
-  function_name          = aws_lambda_function.lambdupdate.function_name
-  maximum_retry_attempts = 0
-
-  destination_config {
-    on_failure {
-      destination = aws_sqs_queue.dlq.arn
-    }
-  }
-}
-
 data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 }
