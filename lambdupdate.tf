b395d5e44431a097b9abf2bc836ea110687f757a
Jacob Luszcz <jacob.luszcz@gmail.com>
2026-07-21 13:09:52 -0400

    chore: remove stale lambdupdate.github.${region} deploy role
    Superseded by lambdupdate.github-deploy.${region}. Retains the github data sources (github_deploy references them).

--- lambdupdate.tf
+++ lambdupdate.tf
@@ -133,41 +133,6 @@ data "aws_iam_policy_document" "github" {
   }
 }
 
-resource "aws_iam_policy" "github" {
-  name   = "lambdupdate.github.${var.aws_region}"
-  policy = data.aws_iam_policy_document.github.json
-}
-
-resource "aws_iam_role" "github" {
-  name = "lambdupdate.github.${var.aws_region}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = data.aws_iam_openid_connect_provider.github.arn
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:jluszcz/LambdUpdate:*"
-          },
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "github" {
-  role       = aws_iam_role.github.name
-  policy_arn = aws_iam_policy.github.arn
-}
-
 resource "aws_iam_policy" "github_deploy" {
   name   = "lambdupdate.github-deploy.${var.aws_region}"
   policy = data.aws_iam_policy_document.github.json
